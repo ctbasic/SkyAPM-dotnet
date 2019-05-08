7c9d04083bb872cad02d03318c0b0723a6970175
温 <ig.start@qq.com>
2019-05-08 16:04:33 +0800

    no message

--- sample2/SkyApm.Sample2.AspNet45Console/Program.cs
+++ sample2/SkyApm.Sample2.AspNet45Console/Program.cs
@@ -14,26 +14,27 @@ namespace SkyApm.Sample2.AspNet45Console
         {
             CtSkyApmNetFxAgent.StartAsync();
 
-            Task.Run(() =>
+
+            while (true)
             {
-
-                while (true)
+                try
                 {
-                    try
-                    {
 
-                        var httpClient = new HttpClient(new HttpTracingHandler());
-                        HttpRequestMessage message = new HttpRequestMessage();
-                        message.RequestUri = new Uri("http://192.168.1.201:5002/home/get");
-                        var reqResult = httpClient.SendAsync(message).Result;
-                        var data2 = reqResult.Content.ReadAsStringAsync().Result;
+                    var httpClient = new HttpClient(new HttpTracingHandler());
+                    HttpRequestMessage message = new HttpRequestMessage();
+                    message.RequestUri = new Uri("http://192.168.1.201:5002/home/get");
+                    var reqResult = httpClient.SendAsync(message).Result;
+                    var data2 = reqResult.Content.ReadAsStringAsync().Result;
 
-                    }
-                    catch { }
-                    System.Threading.Thread.Sleep(10000);
+                    Console.WriteLine(data2);
                 }
+                catch { }
 
-            });
+                Console.ReadLine();
+
+            }
+
+
 
             Console.WriteLine("srtart..");
             Console.Read();
